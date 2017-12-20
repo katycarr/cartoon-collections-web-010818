67fd4dacb8189b4dfe447308857645a459ea2fdd
Katy Carr <khcarr9@gmail.com>
2017-12-20 01:05:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,6 +32,8 @@ def find_the_cheese(array)# code an argument here
   cheese_types.each do |cheese|
     if array.include?(cheese)
       return cheese
+    else
+      return nil
     end
   end
 end
