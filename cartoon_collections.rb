f9d7b88b45889cb20b0f3e280b7d5b47d7a98b87
Katy Carr <khcarr9@gmail.com>
2017-12-20 00:37:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index |index,name| do
+  dwarves.each_with_index do |name, index|
     puts "#{index} #{name}"
   end
 end
